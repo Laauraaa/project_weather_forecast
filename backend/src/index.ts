1a2afc4741787423675176ccd47aff69e25e0eb3
Laura <analaura3rocha@gmail.com>
2025-05-13 20:44:45 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv';  
 dotenv.config();
-
-import express, { Request, Response } from 'express';
-import axios from 'axios';
+import express, { Request, Response } from 'express';  
+import axios from 'axios';  
 import cors from 'cors';
-import path from 'path';
 
-// ✅ __dirname funciona direto em CommonJS
+console.log('API_KEY:', process.env.API_KEY); // Verifique se a variável de ambiente foi carregada corretamente
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,7 +15,7 @@ app.use(express.json());
 app.get('/weather', async (req: Request, res: Response) => {
   const city: string = req.query.city as string;
   const apiKey = process.env.API_KEY;
-
+  
   try {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
@@ -26,15 +25,6 @@ app.get('/weather', async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Erro ao buscar dados da API.' });
   }
 });
-
-// ✅ Serve arquivos estáticos do Angular
-app.use(express.static(path.join(__dirname, '../dist/project_weather_forecast/browser')));
-
-// ✅ Rota fallback (SPA)
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/project_weather_forecast/browser/index.html'));
-});
-
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
